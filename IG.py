6fa0b32403f94145f2dc99e4ff3fc13b83ea5b04
blingstand <blingstand@hotmail.fr>
2019-05-04 11:33:17 +0400

    msg guide entries, clic => clean, bg chg

--- IG.py
+++ IG.py
@@ -3,12 +3,25 @@ from tkinter import messagebox
 
 #message guide sur entries, s'effaçant au clic
 
-#à remplir plus tard
+
 def valider() :
-  pseudo  = str(entry_pseudo.get())
+  pseudo = str(entry_pseudo.get())
   mdp = str(entry_mdp.get())
   messagebox.showinfo("Valider", "Confirmez-vous ceci ?\n\n pseudo = {},\nmot de passe = {}. ".format(pseudo,mdp))
   fenetre.destroy()
+
+couleur_champ_entry = "#fff0b3"
+def clean_entry_pseudo(event):
+  if str(entry_pseudo.get()) == "Entrez votre pseudo ..." :
+    entry_pseudo.delete(0, "end")
+    entry_pseudo.insert(0, "")
+  entry_pseudo.config(bg = couleur_champ_entry)
+
+def clean_entry_mdp(event):
+  if str(entry_mdp.get()) == "Entrez votre mot de passe ..." :
+    entry_mdp.delete(0, "end")
+    entry_mdp.insert(0, "")
+  entry_mdp.config(bg = couleur_champ_entry)
 
 #CSS
 titel_font = "times 24 bold"
@@ -30,11 +43,15 @@ label_titel.pack(padx = 1, pady = 1 )
 #1ere entry
 entry_pseudo = tkinter.Entry(frame_principal, textvariable = "", width = 25, font = entry_font )
 entry_pseudo.insert(0, "Entrez votre pseudo ...")
+entry_pseudo.focus_set()
+entry_pseudo.bind("<Button-1>", clean_entry_pseudo)
 entry_pseudo.pack(padx = 1, pady = 2)
 
 #2e entry
 entry_mdp = tkinter.Entry(frame_principal, width = 25, font = entry_font )
 entry_mdp.insert(0, "Entrez votre mot de passe ...")
+entry_mdp.focus_set()
+entry_mdp.bind("<Button-1>", clean_entry_mdp)
 entry_mdp.pack(padx = 2, pady = 2)
 
 #bouton
