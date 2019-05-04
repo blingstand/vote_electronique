c6012e1bdcae7a35340f509c12a03f1a30cf3241
blingstand <blingstand@hotmail.fr>
2019-05-04 11:02:56 +0400

    explication pour entries non effacableautomatiquement

--- IG.py
+++ IG.py
@@ -1,18 +1,18 @@
 import tkinter
 from tkinter import messagebox
-#les 2 vars de l'alerte recup les résultats de entries
 
+#message guide sur entries, s'effaçant au clic
 
 #à remplir plus tard
 def valider() :
-  nom  = str(entry_nom.get())
+  pseudo  = str(entry_pseudo.get())
   mdp = str(entry_mdp.get())
-  messagebox.showinfo("Valider", "Confirmez-vous ceci ?\n\n nom = {},\nmot de passe = {}. ".format(nom,mdp))
+  messagebox.showinfo("Valider", "Confirmez-vous ceci ?\n\n pseudo = {},\nmot de passe = {}. ".format(pseudo,mdp))
   fenetre.destroy()
 
 #CSS
 titel_font = "times 24 bold"
-entry_font = "times 20 italic"
+entry_font = "times 15 italic"
 button_font = "times 20 bold"
 
 
@@ -28,11 +28,13 @@ label_titel = tkinter.Label(frame_principal, text = "Identification", font = tit
 label_titel.pack(padx = 1, pady = 1 )
 
 #1ere entry
-entry_nom = tkinter.Entry(frame_principal, textvariable = "", width = 15, font = entry_font )
-entry_nom.pack(padx = 1, pady = 2)
+entry_pseudo = tkinter.Entry(frame_principal, textvariable = "", width = 25, font = entry_font )
+entry_pseudo.insert(0, "Entrez votre pseudo ...")
+entry_pseudo.pack(padx = 1, pady = 2)
 
 #2e entry
-entry_mdp = tkinter.Entry(frame_principal, textvariable = "", width = 15, font = entry_font, show = "*"  )
+entry_mdp = tkinter.Entry(frame_principal, width = 25, font = entry_font )
+entry_mdp.insert(0, "Entrez votre mot de passe ...")
 entry_mdp.pack(padx = 2, pady = 2)
 
 #bouton
